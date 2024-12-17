edc6a96658f12233170617be535336de41607397
Mani <mani@adloggs.org>
2024-12-17 13:42:41 +0530

    demo branch push

--- loginsystem/index.php
+++ loginsystem/index.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Home | Registration and Login System </title>
+        <title>Home | Registration and Login System mani</title>
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
     </head>
